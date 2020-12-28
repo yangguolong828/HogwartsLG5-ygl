ef919551f9f44ea2a6a58f456b871ccb86cea620
yangguolong <yangguolong@bytedance.com>
2020-12-29 00:36:17 +0800

    提交文件

---
+++ pytestml/test_fixtrue.py
@@ -0,0 +1,17 @@
+
+
+
+def login():
+    print("登录操作")
+#测试用例1：需要提前登录
+def test_case1():
+    print("测试用例一")
+#测试用例2：不需要提前登录
+def test_case2():
+    print("测试用例二")
+#测试用例3：需要提前登录
+def test_case3():
+    print("测试用例三")
+#测试用例4：不需要提前登录
+def test_case4():
+    print("测试用例四")
